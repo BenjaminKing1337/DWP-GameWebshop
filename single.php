a6466449c470e9eb60e9ddb0453ba5e9c9875427
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2021-10-21 17:48:55 +0200

    all products and single page setup

--- single.php
+++ single.php
@@ -1,9 +1,65 @@
-<?php require_once("./navigation/header.php"); ?>
-
-
 <html>
-    <body>
-        <br><br><br>
-        <div align="center" style="font-size: 50px;">Single Product Page</div>
-    </body>
+<?php include("navigation/header.php"); ?>
+
+<br><br><br>
+<div align="center" style="font-size: 50px;">Single Product Page</div>
+
+<div class="singleProductContainer">
+
+    <div class="thumbnail">
+        <img width="300px" height="200px" src="https://image.api.playstation.com/vulcan/img/cfn/11307DTu3lzL6thuipVwZruYSmRFn1_SpucegJYgtAzcjZLIRPxpCVJkr5C8vfVy5FYMRdHbaJHQXOZldbhjm9ypcA4w51iZ.png" alt="">
+    </div>
+
+    <div class="titlecard">
+        <h2>The Outer Wilds</h2>
+        <h3>PS4</h3>
+    </div>
+    <div class="break"></div>
+    <div class="description">
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus magna turpis, et venenatis nulla interdum quis. Etiam ac enim augue. Ut molestie cursus eros, ac ornare justo cursus vitae. Duis finibus, nulla quis ultrices varius, ante tellus fermentum dolor, eget commodo ante velit id quam. In consequat dui id semper dapibus. Pellentesque eros est, pulvinar a nunc nec, consectetur condimentum felis.</h4>
+    </div>
+    <div class="primarySpecs">
+        <div class="spec">Player Count</div>
+        <div class="spec">Required Space</div>
+        <div class="spec">Online Players</div>
+    </div>
+    <div class="video">
+        <h2>trailer goes here</h2>
+    </div>
+    <div class="slideshow">
+        <h2>images for game go here</h2>
+    </div>
+    <div class="secondarySpecs">
+        <table>
+            <tr>
+                <th>Age Rating</th>
+                <td>18 +</td>
+            </tr>
+            <tr>
+                <th>ESRB Rating</th>
+                <td>M</td>
+            </tr>
+            <tr>
+                <th>Multiplayer</th>
+                <td>Yes</td>
+            </tr>
+            <tr>
+                <th>In-game Purchases</th>
+                <td>Yes</td>
+            </tr>
+        </table>
+    </div>
+    <div class="productPricing">
+        <h5>Old Price</h5>
+        <h2>New Price</h2>
+    </div>
+    <div class="addToBasket">
+        <button><p>Add To Basket</p> <i class="materialize-icon "><span class="material-icons">shopping_cart
+                </span></i></button>
+    </div>
+
+</div>
+
+<?php include("navigation/footer.php"); ?>
+
 </html>
