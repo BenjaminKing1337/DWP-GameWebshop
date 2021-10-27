45115238b6420f698f60cdbf2cda3fe8f485c78e
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2021-10-27 18:42:08 +0200

    up

--- navigation/header.php
+++ navigation/header.php
@@ -2,7 +2,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="../../DWP-GameWebshop/styles/style.css">
+    <link rel="stylesheet" href="/DWP-GameWebshop/styles/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
@@ -12,20 +12,20 @@
         <input type="checkbox" id="check2">
 
         <div class="logo">
-            <img onclick="window.location='../../DWP-GameWebshop/index.php';" src="../../DWP-GameWebshop/assets/logo.png" alt="DWP" height="65px">
+            <img onclick="window.location='/DWP-GameWebshop/index.php';" src="/DWP-GameWebshop/assets/logo.png" alt="DWP" height="65px">
             <h1>DWP Games</h1>
         </div>
         <ul class="platformMenu">
             <div class="dropdown">
                 <li class="desktop_link"><a href="#">Platform</a></li>
                 <div class="dropdown-content">
-                    <li><a href="../../DWP-GameWebshop/all.php">PC</a></li>
+                    <li><a href="/DWP-GameWebshop/all.php">PC</a></li>
                     <li><a href="#">Playstation</a></li>
                     <li><a href="#">Xbox</a></li>
                     <li><a href="#">Nintendo</a></li>
                 </div>
             </div>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/all.php">PC</a></li>
+            <li class="mobile_link"><a href="/DWP-GameWebshop/all.php">PC</a></li>
             <li class="mobile_link"><a href="#">Playstation</a></li>
             <li class="mobile_link"><a href="#">Xbox</a></li>
             <li class="mobile_link"><a href="#">Nintendo</a></li>
@@ -37,18 +37,18 @@
             <div class="dropdown">
                 <li class="desktop_link"><a href="#">User</a></li>
                 <div class="dropdown-content">
-                    <li><a href="../../DWP-GameWebshop/users/newuser.php">Create New User</a></li>
-                    <li><a href="../../DWP-GameWebshop/users/accounts.php">User List</a></li>
-                    <li><a href="../../DWP-GameWebshop/users/admin/addproduct.php">Add Product</a></li>
-                    <li><a href="../../DWP-GameWebshop/users/login.php">Login</a></li>
-                    <li><a href="../../DWP-GameWebshop/users/logout.php">Logout</a></li>
+                    <li><a href="/DWP-GameWebshop/users/newuser.php">Create New User</a></li>
+                    <li><a href="/DWP-GameWebshop/users/accounts.php">User List</a></li>
+                    <li><a href="=/DWP-GameWebshop/users/admin/addproduct.php">Add Product</a></li>
+                    <li><a href="=/DWP-GameWebshop/users/login.php">Login</a></li>
+                    <li><a href="=/DWP-GameWebshop/users/logout.php">Logout</a></li>
                 </div>
             </div>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/users/newuser.php">Create New User</a></li>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/users/accounts.php">User List</a></li>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/users/admin/addproduct.php">Add Product</a></li>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/users/login.php">Login</a></li>
-            <li class="mobile_link"><a href="../../DWP-GameWebshop/users/logout.php">Logout</a></li>
+            <li class="mobile_link"><a href="=/DWP-GameWebshop/users/newuser.php">Create New User</a></li>
+            <li class="mobile_link"><a href="/DWP-GameWebshop/users/accounts.php">User List</a></li>
+            <li class="mobile_link"><a href="/DWP-GameWebshop/users/admin/addproduct.php">Add Product</a></li>
+            <li class="mobile_link"><a href="/DWP-GameWebshop/users/login.php">Login</a></li>
+            <li class="mobile_link"><a href="/DWP-GameWebshop/users/logout.php">Logout</a></li>
         </ul>
 
         <div class="icons">
