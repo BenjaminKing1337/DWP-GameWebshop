bfc74267add6f50270a9fc633060e83a77d21ece
SamoK99 <56873602+SamoK99@users.noreply.github.com>
2021-10-27 15:57:41 +0200

    update

--- all.php
+++ all.php
@@ -1,7 +1,7 @@
 <?php 
 require_once("../DWP-GameWebshop/users/includes/connection.php");
 
-$sql = 'SELECT Price FROM product';
+$sql = 'SELECT id, Price FROM product';
 
 $result = mysqli_query($connection, $sql);
 
@@ -25,14 +25,14 @@ mysqli_close($connection)
     <?php foreach ($products as $product) { ?>
 
         <div class="productContainer">
-            <div onclick="window.location='single.php';" class="product">
+            <div onclick="window.location='single.php?id=<?php echo $product['id']?>;'" class="product">
                 <div class="platformTag">
                     <img width="25px" height="25px" src="https://i.kym-cdn.com/entries/icons/original/000/012/368/playstation-wallpaper-46825-48282-hd-wallpapers.jpg" alt="">
                 </div>
                 <img width="190px" height="180px" src="https://image.api.playstation.com/vulcan/img/cfn/11307DTu3lzL6thuipVwZruYSmRFn1_SpucegJYgtAzcjZLIRPxpCVJkr5C8vfVy5FYMRdHbaJHQXOZldbhjm9ypcA4w51iZ.png" alt="">
             </div>
             <div class="price">
-                <h3><?php echo htmlspecialchars($product['Price']); ?></h3> <br>
+                <h3><?php echo htmlspecialchars($product['Price']); ?> DKK</h3> <br>
                 <h4>Old Price</h4>
                 <button>Add <br>
                     to <br>
--- single.php
+++ single.php
@@ -1,64 +1,87 @@
+<?php
+require_once("../DWP-GameWebshop/users/includes/connection.php");
+
+//check id parameter
+if(isset($_GET['id'])){
+    $id = mysqli_real_escape_string($connection, $_GET['id']);
+
+    $sql = "SELECT * FROM product WHERE id = $id";
+
+    $result = mysqli_query($connection, $sql);
+
+    $product = mysqli_fetch_assoc($result);
+
+    mysqli_free_result($result);
+    mysqli_close($connection);
+
+
+}
+
+?>
+
 <html>
 <?php include("navigation/header.php"); ?>
 
 <br><br><br>
-<div align="center" style="font-size: 50px;">Single Product Page</div>
 
-<div class="singleProductContainer">
+<?php if($product): ?>
+    <div class="singleProductContainer">
 
-    <div class="thumbnail">
-        <img width="300px" height="200px" src="https://image.api.playstation.com/vulcan/img/cfn/11307DTu3lzL6thuipVwZruYSmRFn1_SpucegJYgtAzcjZLIRPxpCVJkr5C8vfVy5FYMRdHbaJHQXOZldbhjm9ypcA4w51iZ.png" alt="">
-    </div>
+        <div class="thumbnail">
+            <img width="300px" height="200px" src="https://image.api.playstation.com/vulcan/img/cfn/11307DTu3lzL6thuipVwZruYSmRFn1_SpucegJYgtAzcjZLIRPxpCVJkr5C8vfVy5FYMRdHbaJHQXOZldbhjm9ypcA4w51iZ.png" alt="">
+        </div>
 
-    <div class="titlecard">
-        <h2>The Outer Wilds</h2>
-        <h3>PS4</h3>
+        <div class="titlecard">
+            <h2><?php echo htmlspecialchars($product['Title']); ?></h2>
+            <h3><?php echo htmlspecialchars($product['Platform']); ?></h3>
+        </div>
+        <div class="break"></div>
+        <div class="description">
+            <h4><?php echo htmlspecialchars($product['Description']); ?></h4>
+        </div>
+        <div class="primarySpecs">
+            <div class="spec">Player Count</div>
+            <div class="spec">Required Space</div>
+            <div class="spec">Online Players</div>
+        </div>
+        <div class="video">
+            <h2>trailer goes here</h2>
+        </div>
+        <div class="slideshow">
+            <h2>images for game go here</h2>
+        </div>
+        <div class="secondarySpecs">
+            <table>
+                <tr>
+                    <th>Age Rating</th>
+                    <td>18 +</td>
+                </tr>
+                <tr>
+                    <th>ESRB Rating</th>
+                    <td>M</td>
+                </tr>
+                <tr>
+                    <th>Multiplayer</th>
+                    <td>Yes</td>
+                </tr>
+                <tr>
+                    <th>In-game Purchases</th>
+                    <td>Yes</td>
+                </tr>
+            </table>
+        </div>
+        <div class="productPricing">
+            <h5>Old Price</h5>
+            <h2><?php echo htmlspecialchars($product['Price']); ?></h2>
+        </div>
+        <div class="addToBasket">
+            <button><p>Add To Basket</p> <i class="materialize-icon "><span class="material-icons">shopping_cart
+                    </span></i></button>
+        </div>
     </div>
-    <div class="break"></div>
-    <div class="description">
-        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc finibus magna turpis, et venenatis nulla interdum quis. Etiam ac enim augue. Ut molestie cursus eros, ac ornare justo cursus vitae. Duis finibus, nulla quis ultrices varius, ante tellus fermentum dolor, eget commodo ante velit id quam. In consequat dui id semper dapibus. Pellentesque eros est, pulvinar a nunc nec, consectetur condimentum felis.</h4>
-    </div>
-    <div class="primarySpecs">
-        <div class="spec">Player Count</div>
-        <div class="spec">Required Space</div>
-        <div class="spec">Online Players</div>
-    </div>
-    <div class="video">
-        <h2>trailer goes here</h2>
-    </div>
-    <div class="slideshow">
-        <h2>images for game go here</h2>
-    </div>
-    <div class="secondarySpecs">
-        <table>
-            <tr>
-                <th>Age Rating</th>
-                <td>18 +</td>
-            </tr>
-            <tr>
-                <th>ESRB Rating</th>
-                <td>M</td>
-            </tr>
-            <tr>
-                <th>Multiplayer</th>
-                <td>Yes</td>
-            </tr>
-            <tr>
-                <th>In-game Purchases</th>
-                <td>Yes</td>
-            </tr>
-        </table>
-    </div>
-    <div class="productPricing">
-        <h5>Old Price</h5>
-        <h2>New Price</h2>
-    </div>
-    <div class="addToBasket">
-        <button><p>Add To Basket</p> <i class="materialize-icon "><span class="material-icons">shopping_cart
-                </span></i></button>
-    </div>
-
-</div>
+<?php else: ?>
+    <div align="center" style="font-size: 50px;">No such product exists!</div>
+<?php endif; ?>
 
 <?php include("navigation/footer.php"); ?>
 
