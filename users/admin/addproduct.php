032577ff6b88f449e66e44556cf37d166091274b
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2021-11-02 17:53:15 +0100

    update to add product

--- users/admin/addproduct.php
+++ users/admin/addproduct.php
@@ -1,9 +1,33 @@
+<?php
+
+require_once('../includes/connection.php');
+
+if (isset($_POST['submit'])) {
+    $Title = $_POST['Title'];
+    $Price = $_POST['Price'];
+    $ReleaseDate = $_POST['ReleaseDate'];
+    $Description = $_POST['Description'];
+    $Rating = $_POST['Rating'];
+    $Platform = $_POST['Platform'];
+
+    $query = "INSERT INTO `product` (`id`, `Title`, `Price`, `ReleaseDate`, `Description`, `Rating`, `Platform`) VALUES (NULL, '$Title', '$Price', '$ReleaseDate', '$Description', '$Rating', '$Platform')";
+
+    if (!mysqli_query($connection, $query)) {
+        die("DB error: " . mysqli_error($connection));
+    } else {
+        header("Location:addproduct.php");
+        echo "Product Added";
+    }
+}
+?>
+
+
 <html>
 <?php include('../../../DWP-GameWebshop/navigation/header.php'); ?>
 
 <div class="addProductContainer">
-    <h2>Here you can add a new product.</h2>    
-<form method="post" action="addPtoDB.php">
+    <h2>Here you can add a new product.</h2>
+    <form method="post" action="addproduct.php">
         Title:<br><input type="text" name="Title"> <br><br>
         Price:<br><input type="text" name="Price"> <br><br>
         Release Date:<br><input type="text" name="ReleaseDate"> <br><br>
