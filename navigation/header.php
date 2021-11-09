59143944cb95525e3973dcbd528bec135e5e5022
BenjaminKing1337 <BenjaminKing1337@users.noreply.github.com>
2021-11-09 16:35:28 +0100

    ****ING FINALLY!!!!!!!!!! ***K

--- navigation/header.php
+++ navigation/header.php
@@ -1,13 +1,12 @@
-
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/DWP-GameWebshop/styles/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<!--     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
- -->    <script src="../main.js"></script>
+    <!--     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+ -->
+    <script src="../main.js"></script>
 </head>
 
 <body>
@@ -37,7 +36,7 @@
             <li><a href="/DWP-GameWebshop/all.php">Genres</a></li>
         </ul>
 
-        <ul class="userMenu"  id="userMenu">
+        <ul class="userMenu" id="userMenu">
             <div class="dropdown">
                 <li class="desktop_link"><a href="#">User</a></li>
                 <div class="dropdown-content">
@@ -58,27 +57,34 @@
         <div class="icons">
             <span class="material-icons icon cart">shopping_cart</span>
             <label for="check" id="addlayer1" class="material-icons icon menu">menu</label>
-            <label for="check2"  id="addlayer2"  class="material-icons icon menu">account_circle</label>
+            <label for="check2" id="addlayer2" class="material-icons icon menu">account_circle</label>
         </div>
     </header>
 
 
     <script>
 
-    document.getElementById("addlayer1").addEventListener("click", function() {
-     console.log("lameo")
-
-        var element = document.getElementById("platformMenu");
-        element.classList.toggle("addlayer");
-    });
 
 
-    document.getElementById("addlayer2").addEventListener("click", function() {
-     console.log("lameo")
+        document.getElementById("addlayer1").addEventListener("click", function() {
+            console.log("lameo")
 
-        var element = document.getElementById("userMenu");
-        element.classList.toggle("addlayer");
-    });
+            var element = document.getElementById("platformMenu");
+            element.classList.toggle("addlayer");
+            var element = document.getElementById("userMenu");
+            element.classList.remove("addlayer");
+            var element = document.getElementById("check2").checked = false;
+        });
 
 
+        document.getElementById("addlayer2").addEventListener("click", function() {
+            console.log("lameo")
+
+            var element = document.getElementById("userMenu");
+            element.classList.toggle("addlayer");
+            var element = document.getElementById("platformMenu");
+            element.classList.remove("addlayer");
+            var element = document.getElementById("check").checked = false;
+           
+        });
     </script>
